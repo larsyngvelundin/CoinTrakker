163be38b765a4d1a23857ac84f15cd21c5a2f96b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-13 23:38:28 +0100

    First Successful connection

--- btc_test.py
+++ btc_test.py
@@ -1,5 +1,5 @@
 from decouple import config
-
+from bitcoinrpc.authproxy import AuthServiceProxy, JSONRPCException
 
 
 #Import all environment variables:
@@ -9,3 +9,8 @@ rpc_port = config('rpc_port')
 rpc_url = config('rpc_url')
 
 print("Imports done!")
+
+
+rpc_connection = AuthServiceProxy(f"http://{rpc_username}:{rpc_password}@{rpc_url}:{rpc_port}")
+best_block_hash = rpc_connection.getbestblockhash()
+print(rpc_connection.getblock(best_block_hash))
