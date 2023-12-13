bf8944c8412a1fba7d8e60dbace2a8536a19dd3b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-13 23:34:04 +0100

    Create btc_test.py

---
+++ btc_test.py
@@ -0,0 +1,11 @@
+from decouple import config
+
+
+
+#Import all environment variables:
+rpc_username = config('rpc_username')
+rpc_password = config('rpc_password')
+rpc_port = config('rpc_port')
+rpc_url = config('rpc_url')
+
+print("Imports done!")
