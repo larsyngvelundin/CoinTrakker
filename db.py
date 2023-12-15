4aeb3861349750ec6e4c6e91874ac3a3a61605a3
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-15 19:16:11 +0100

    Update db.py

--- db.py
+++ db.py
@@ -1,26 +1,27 @@
-import sqlite3
+import db_files.create_db
+import db_files.db_exists
+import db_files.save_transaction
 
 #Ensure that db folders exists and create them if not
 
 #check_balance(address, block=latest)
     #check if file exists in local_db/incoming
         #get sum of amount up until block
+        #SUM amount WHERE block LESS THAN {block+1}
     #check if file exists in local_deb/outgoing
         #subtract amount from sum up until block
 
-#createdb
-#def create_db(address):
-#   con = sqlite3.connect("local_db/outgoing/{address}.db")
-#   create table transactions(amount to block)
-#   con = sqlite3.connect("local_db/incoming/{address}.db")
-#   create table transactions(amount from block)
+#checkifdbexists
+def db_exists(address):
+   return db_files.db_exists.main(address)
 
+#createdb
+def create_db(address):
+    db_files.create_db.main(address)
 #get_transactions_from(from, from_block, to_block)
 
 #get_transactions_to(from, from_block, to_block)
 
-#save_transaction(amount, to, from, block)
-    #con = sqlite3.connect("local_db/outgoing/{from}.db")
-        #save amount and to and block
-    #con = sqlite3.connect("local_db/incoming/{to}.db")
-        #save amount and from and block
+# savetransaction
+def save_transaction(amount, to_address, from_address, block):
+    return db_files.save_transaction.main()
