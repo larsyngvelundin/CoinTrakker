0963b7de08c1e878cade240548f51f4928d677af
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-16 15:33:20 +0100

    Create initialize.py

---
+++ db_files/initialize.py
@@ -0,0 +1,32 @@
+from loguru import logger
+import os
+# import os.path
+import sqlite3
+
+def main():
+    logger.info("Test")
+    #check if local_db folder exists
+    if not os.path.isdir("local_db"):
+        os.mkdir("local_db")
+        os.mkdir("local_db/incoming")
+        os.mkdir("local_db/outgoing")
+    #create wallets.db/addresses.db
+    con = sqlite3.connect("local_db/addresses.db")
+    with con:
+        con.execute("""
+        CREATE TABLE if not exists addresses(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            address TEXT UNIQUE
+        );
+        """)
+
+    con = sqlite3.connect("local_db/transaction_hashes.db")
+    with con:
+        con.execute("""
+        CREATE TABLE if not exists transaction_hashes(
+            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+            transaction_hash TEXT UNIQUE
+        );
+        """)
+    pass
+
