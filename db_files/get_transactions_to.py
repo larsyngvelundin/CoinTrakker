d53a5cd09c9de244e4d1f92b476fa9b93247ba35
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-16 15:33:23 +0100

    Create get_transactions_to.py

---
+++ db_files/get_transactions_to.py
@@ -0,0 +1,15 @@
+from loguru import logger
+import sqlite3
+
+def main(address, from_block=0, to_block=100):
+    balance = 0
+    logger.info("checking to {address}")
+    sql = f"SELECT amount FROM transactions WHERE block BETWEEN {from_block} and {to_block}"
+    con = sqlite3.connect(f"local_db/incoming/{address}.db")
+    with con:
+        data = con.execute(sql)
+    outgoing = data.fetchall()
+    for transaction in outgoing:
+        balance += transaction[0]
+    # logger.info(balance)
+    return balance
