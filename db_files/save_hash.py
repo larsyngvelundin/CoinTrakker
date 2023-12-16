668965a1ab6990df497b5f7721fe7c6ddcfe272b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-16 15:33:11 +0100

    Create save_hash.py

---
+++ db_files/save_hash.py
@@ -0,0 +1,24 @@
+from loguru import logger
+import sqlite3
+
+def main(transaction_hash):
+    logger.info("test")
+    con = sqlite3.connect("local_db/transaction_hashes.db")
+    #Try to save the hash
+    try:
+        cursor = con.cursor()
+        sql = f'INSERT INTO transaction_hashes (transaction_hash) VALUES (?)'
+        cursor.execute(sql, (transaction_hash,))
+        con.commit()
+    except Exception as error_msg:
+        logger.error(f"Could not save {transaction_hash} (probably already saved)")
+        logger.error(error_msg)
+    
+    #Get and return the hash
+    sql = f"SELECT id FROM transaction_hashes WHERE transaction_hash='{transaction_hash}';"
+    with con:
+        data = con.execute(sql)
+        logger.info(f"sql data: {data}")
+    data = data.fetchone()[0]
+    con.close()
+    return data
