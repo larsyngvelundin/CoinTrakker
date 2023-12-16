1e5ccc8be3ac91a61b66954c9996b6e201eac99b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-16 15:33:06 +0100

    Update save_transaction.py

--- db_files/save_transaction.py
+++ db_files/save_transaction.py
@@ -9,16 +9,26 @@ def main(amount, to_address, from_address, block, transaction_hash):
         db.create_db(to_address)
         logger.info(f"Created DB files")
 
+    #Save/get the address IDs
+    from_address_id = db.save_address(from_address) 
+    to_address_id = db.save_address(to_address) 
+
+    #Save/get the transaction ID
+    transaction_hash_id = db.save_hash(transaction_hash)
+
+    #Save outgoing for sender
     con = sqlite3.connect(f"local_db/outgoing/{from_address}.db")
     sql =  f'INSERT INTO transactions (amount, to_address, block, transaction_hash) values(?,?,?,?)'
-    data = [(amount, to_address, block, transaction_hash)]
+    data = [(amount, to_address_id, block, transaction_hash_id)]
     with con:
         con.executemany(sql, data)
     logger.info(f"Added {transaction_hash} to outgoing/{from_address}.db")
 
+    #Save incoming for recipient
     con = sqlite3.connect(f"local_db/incoming/{to_address}.db")
     sql =  f'INSERT INTO transactions (amount, from_address, block, transaction_hash) values(?,?,?,?)'
-    data = [(amount, from_address, block, transaction_hash)]
+    data = [(amount, from_address_id, block, transaction_hash_id)]
     with con:
         con.executemany(sql, data)
     logger.info(f"Added {transaction_hash} to incoming/{to_address}.db")
+
