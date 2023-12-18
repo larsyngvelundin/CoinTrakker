0bf86ca9fe97e0dbe7c1de9803f23741551e3383
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-18 13:57:05 +0100

    Error handling for non-plain addresses

--- rpc_files/get_sender_address.py
+++ rpc_files/get_sender_address.py
@@ -12,6 +12,19 @@ def main(con, transaction_hash):
         prev_tx = rpc.get_raw_transaction(prev_tx_hash)
         prev_tx_decoded = rpc.decode_raw_transaction(prev_tx)
         logger.debug(f"Decoded prev: \n{prev_tx_decoded}")
-        sender_address = prev_tx_decoded['vout'][vout_index]['scriptPubKey']['address']
-        logger.debug(f"Sender addresses: {sender_address}")
+        try:
+            sender_address = prev_tx_decoded['vout'][vout_index]['scriptPubKey']['address']
+            logger.debug(f"Sender addresses: {sender_address}")
+        except KeyError as error_msg:
+            logger.debug("No plain address address found")
+            try:
+                pubkey = item['scriptPubKey']['asm'].split(" ")[0]
+                to_address = rpc.get_address_from_pubkey(pubkey)
+                amount = item['value']
+                logger.info(f"{item['value']} rewarded to {to_address}")
+                parsed['to'][to_address] = amount
+                total_amount += amount
+            except KeyError:
+                logger.error("Could not find any address for this transaction")
+                logger.info(f"Transaction vout:\n{decoded_transaction[key]}")
         return sender_address
