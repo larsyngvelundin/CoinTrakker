d9a2f72f4cf36711c6f671781a397eef85838a2e
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-20 00:14:45 +0100

    Create get_hash_from_id.py

---
+++ db_files/get_hash_from_id.py
@@ -0,0 +1,12 @@
+from loguru import logger
+import sqlite3
+
+def main(id):
+    logger.info("test")
+
+    con = sqlite3.connect(f"local_db/transaction_hashes.db")
+    sql = f"SELECT transaction_hash FROM transaction_hashes WHERE id = '{id}';"
+    with con:
+        data = con.execute(sql)
+    transaction_hash = data.fetchone()[0]
+    return transaction_hash
