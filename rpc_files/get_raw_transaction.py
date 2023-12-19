4ad032f25e9a128473385824dcf52c88ea4e8880
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-20 00:45:51 +0100

    Update get_raw_transaction.py

--- rpc_files/get_raw_transaction.py
+++ rpc_files/get_raw_transaction.py
@@ -1,4 +1,4 @@
 from loguru import logger
-def main(con, tx_hash):
+def main(con, tx_hash, detailed=False):
     logger.debug("test")
-    return con.getrawtransaction(tx_hash)
+    return con.getrawtransaction(tx_hash, detailed)
