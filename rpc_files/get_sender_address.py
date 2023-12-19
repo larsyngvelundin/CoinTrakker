b35a0aa7bf9a6fa1f0f49503f9473d1c4e0c60b7
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-19 19:38:19 +0100

    Update get_sender_address.py

--- rpc_files/get_sender_address.py
+++ rpc_files/get_sender_address.py
@@ -13,18 +13,25 @@ def main(con, transaction_hash):
         prev_tx_decoded = rpc.decode_raw_transaction(prev_tx)
         logger.debug(f"Decoded prev: \n{prev_tx_decoded}")
         try:
+            logger.debug(f"vout: {prev_tx_decoded['vout']}")
+            logger.debug(f"vout[{vout_index}]: {prev_tx_decoded['vout'][vout_index]}")
             sender_address = prev_tx_decoded['vout'][vout_index]['scriptPubKey']['address']
             logger.debug(f"Sender addresses: {sender_address}")
         except KeyError as error_msg:
             logger.debug("No plain address address found")
             try:
-                pubkey = item['scriptPubKey']['asm'].split(" ")[0]
-                to_address = rpc.get_address_from_pubkey(pubkey)
-                amount = item['value']
-                logger.info(f"{item['value']} rewarded to {to_address}")
-                parsed['to'][to_address] = amount
-                total_amount += amount
-            except KeyError:
+                logger.debug(f"Trying to access {prev_tx_decoded}")
+                pubkey = prev_tx_decoded['vout'][vout_index]['scriptPubKey']['asm'].split(" ")[0]
+                logger.debug("Trying")
+                sender_address = rpc.get_address_from_pubkey(pubkey)
+                logger.debug("Trying")
+                # amount = prev_tx_decoded['value']
+                logger.debug("Trying")
+                # logger.info(f"{prev_tx_decoded['value']} rewarded to {to_address}")
+                # parsed['to'][to_address] = amount
+                # total_amount += amount
+            except KeyError as error_msg:
                 logger.error("Could not find any address for this transaction")
-                logger.info(f"Transaction vout:\n{decoded_transaction[key]}")
+                logger.error(error_msg)
+                logger.info(f"Transaction vout:\n{decoded_transaction['vout']}")
         return sender_address
