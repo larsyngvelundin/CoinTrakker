8ce415fc105a2c1b668c01774997f573c0df72f5
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-20 22:27:24 +0100

    moved old test files from root

--- old_test_files/rpc_test.py
+++ old_test_files/rpc_test.py
@@ -2,7 +2,14 @@
 
 import rpc
 import db
+import convert
 
+import sys
+from loguru import logger
+
+logger.remove()
+logger.add(sys.stderr, level="INFO")
+logger.add("ct.log", rotation="500 MB")
 
 # test = rpc.parse_transaction_data(tx)
 
@@ -15,4 +22,9 @@ import db
 # print(f"Full transaction:\n{test}")
 # print(f"Balance: {bal}")
 
-rpc.parse_blocks(170,1000)
+# rpc.parse_blocks(2634,2634)
+# rpc.parse_blocks(2922,2922)
+# rpc.parse_blocks(222453,222453)
+
+# balance = convert.to_btc(db.check_balance("1ExF15z6YxFrNZHcnH2sYohbKBh47b1aow"))
+# print(f"Balance: {balance}")
