2e053a2d2657f8e1543aa506b935717c3296fb4b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-20 15:43:21 +0100

    Automatic for SQL ID/real data

--- db_files/get_transactions_from.py
+++ db_files/get_transactions_from.py
@@ -1,8 +1,12 @@
 from loguru import logger
 import sqlite3
 
+import db
+
 def main(address, from_block=0, to_block=100):
     balance = 0
+    if(isinstance(address, int)):
+        address = db.get_address_from_id(address)
     logger.debug(f"checking from {address}")
     sql = f"SELECT amount FROM transactions WHERE block BETWEEN {from_block} and {to_block}"
     con = sqlite3.connect(f"local_db/outgoing/{address}.db")
@@ -12,5 +16,4 @@ def main(address, from_block=0, to_block=100):
     for transaction in outgoing:
         logger.debug(transaction)
         balance -= transaction[0]
-    # logger.debug(balance)
     return balance
--- db_files/get_transactions_to.py
+++ db_files/get_transactions_to.py
@@ -1,8 +1,12 @@
 from loguru import logger
 import sqlite3
 
+import db
+
 def main(address, from_block=0, to_block=100):
     balance = 0
+    if(isinstance(address, int)):
+        address = db.get_address_from_id(address)
     logger.debug(f"checking to {address}")
     sql = f"SELECT amount FROM transactions WHERE block BETWEEN {from_block} and {to_block}"
     con = sqlite3.connect(f"local_db/incoming/{address}.db")
