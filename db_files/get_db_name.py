aa386fd0ba3fa48f83e13e688414ef85c58d7d4b
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2023-12-26 13:21:46 +0100

    Increased db name length to 3

--- db_files/get_db_name.py
+++ db_files/get_db_name.py
@@ -1,5 +1,5 @@
 def main(address):
     db_name = address.lower()
     if (db_name != "Mining reward" and db_name != "non-standard" and db_name != "Network Fee"):
-        db_name = address.lower()[:2]
+        db_name = address.lower()[:3]
     return db_name
