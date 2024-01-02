014c08f2bdf851ee64093f026945a3c73aff8182
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-01-02 21:20:42 +0100

    Updated for new DB system

--- db_files/initialize.py
+++ db_files/initialize.py
@@ -3,30 +3,19 @@ import os
 import sqlite3
 
 def main():
-    logger.debug("Test")
-    #check if local_db folder exists
+    logger.debug("Initializing DB")
     if not os.path.isdir("local_db"):
         os.mkdir("local_db")
-        # os.mkdir("local_db/incoming")
-        # os.mkdir("local_db/outgoing")
-    #create wallets.db/addresses.db
-    con = sqlite3.connect("local_db/addresses.db")
+
+    con = sqlite3.connect("local_db/transactions.db")
     with con:
         con.execute("""
-        CREATE TABLE if not exists addresses(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            address TEXT UNIQUE
+        CREATE TABLE if not exists transactions(
+            amount INTEGER,
+            from_address TEXT,
+            to_address TEXT,
+            block INTEGER,
+            transaction_hash TEXT,
+            UID TEXT UNIQUE
         );
         """)
-
-    con = sqlite3.connect("local_db/transaction_hashes.db")
-    with con:
-        con.execute("""
-        CREATE TABLE if not exists transaction_hashes(
-            id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
-            transaction_hash TEXT UNIQUE
-        );
-        """)
-    
-    pass
-
