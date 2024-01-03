8c4755a8c3d3e9affa953f479271c88b632ed321
larsyngvelundin <34173819+larsyngvelundin@users.noreply.github.com>
2024-01-03 13:57:00 +0100

    updated for new db

--- db_files/get_latest_parsed_block.py
+++ db_files/get_latest_parsed_block.py
@@ -5,21 +5,26 @@ import db
 import rpc
 
 def main():
-    logger.debug("Defunct for now")
+    logger.debug("Getting last parsed block")
     # db_file = db.get_db_name("Mining reward")
-    # con = sqlite3.connect(f"local_db/outgoing.db")
-    # sql = 'SELECT MAX(block) from "Mining reward"'
-    # with con:
-    #     data = con.execute(sql)
+    con = sqlite3.connect(f"local_db/transactions.db")
+    cursor = con.cursor()
+    sql = """
+            SELECT MAX(block)
+            FROM transactions
+        """
+    with con:
+        data = cursor.execute(sql)
     # # print(data[0])
-    # block_height = data.fetchone()[0]
+    block_height = data.fetchone()[0]
+    logger.info(block_height)
     # #get block time
-    # try:
-    #     block_time = rpc.get_block_time(block_height)
-    # except ConnectionRefusedError:
-    #     logger.error("Failed to get blocktime, setting 0")
-    #     block_time = 0
+    try:
+        block_time = rpc.get_block_time(block_height)
+    except ConnectionRefusedError:
+        logger.error("Failed to get blocktime, setting 0")
+        block_time = 0
 
-    # block_info = {'time': block_time, 'height': block_height}
+    block_info = {'time': block_time, 'height': block_height}
 
-    # return block_info
+    return block_info
